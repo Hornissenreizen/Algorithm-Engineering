ef09b676c3e90c8c0332ab41247a9bae48940874
Hornissenreizen <horst.121234e@gmail.com>
2024-11-19 09:37:08 +0100

    Week 5

---
+++ Exam Assignments/Code/05/Catch2/CMakeFiles/catch2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/catch2.dir/catch_main.cpp.o"
+  "CMakeFiles/catch2.dir/catch_main.cpp.o.d"
+  "CMakeFiles/catch2.dir/catch_tests_fizzbuzz.cpp.o"
+  "CMakeFiles/catch2.dir/catch_tests_fizzbuzz.cpp.o.d"
+  "catch2"
+  "catch2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/catch2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
